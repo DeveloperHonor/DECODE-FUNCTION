9ce99436bc0fc713f7dac00987e406206580d10e
OpenSourceLinux <62589432+OpenSourceLinux@users.noreply.github.com>
2020-06-04 20:40:01 +0800

    Update decode.sql

--- decode.sql
+++ decode.sql
@@ -1,28 +1,3 @@
-CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text)
-RETURNS text
-AS '../lib/decode.so', 'decode'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text, anyelement, text, anyelement, text, text)
-RETURNS text
-AS '../lib/decode.so', 'decode'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text, text)
-RETURNS text
-AS '../lib/decode.so', 'decode'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text, anyelement, text)
-RETURNS text
-AS '../lib/decode.so', 'decode'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text, anyelement, text, text)
-RETURNS text
-AS '../lib/decode.so', 'decode'
-LANGUAGE C STRICT;
-
 CREATE OR REPLACE FUNCTION decode(anyelement, anyelement, text, anyelement, text, anyelement, text)
 RETURNS text
 AS '../lib/decode.so', 'decode'
